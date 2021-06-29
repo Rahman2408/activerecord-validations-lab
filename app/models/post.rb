3a2f7689839aed208347ea6b6dad8f0757568c22
Rahman Elsirafy <rahman2408@gmail.com>
2021-06-29 00:24:46 -0400

    commit tree c7c768f4bd1e53ea3a634d8c531a4ad58e57e8aa

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction) }
+  validate :clickbait 
+
+    private
+    
+    def clickbait
+       if self.title
+        unless self.title.match?(/Won't Believe | Secret | Top | Guess/) 
+            self.errors.add(:clickbait, "Not clickbait-y enough")
+        end
+        end
+    end
 end
